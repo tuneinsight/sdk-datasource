338167887840161320bdb1b8036e15723ff7ba14
Boris Flesch <boris.flesch@gmail.com>
2023-08-10 18:26:54 +0200

    teleport cert interface

--- pkg/sdk/api_config.go
+++ pkg/sdk/api_config.go
@@ -5,13 +5,17 @@ package sdk
 type APIConfig interface {
 	// APIName should return the name of the for example "misp" or "generic"
 	APIName() string
+	// GetURL should return URL of the API
 	GetURL() string
+	// GetTeleportCert should return the name of the Teleport certificate used for this datasource (if applicable, otherwise "")
+	GetTeleportCert() string
 }
 
 // GenericAPIConfig is the configuration for a generic HTTP API
 type GenericAPIConfig struct {
-	URL   string `yaml:"api-url" json:"api-url" default:"localhost"`
-	Token string `yaml:"api-token" json:"api-token" default:""`
+	URL          string `yaml:"api-url" json:"api-url" default:"localhost"`
+	Token        string `yaml:"api-token" json:"api-token" default:""`
+	TeleportCert string `yaml:"teleport-cert" json:"teleport-cert" default:""`
 }
 
 // APIName returns the name of the API which is 'generic'
@@ -22,4 +26,9 @@ func (conf GenericAPIConfig) APIName() string {
 // GetURL returns the url in configuration
 func (conf GenericAPIConfig) GetURL() string {
 	return conf.URL
+}
+
+// GetTeleportCert returns the name of the Teleport certificate used for this datasource (if applicable)
+func (conf GenericAPIConfig) GetTeleportCert() string {
+	return conf.TeleportCert
 }
